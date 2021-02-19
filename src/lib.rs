43a14ff1269488138b151b4785a4b797212a2b78
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2021-02-18 23:23:00 -0500

    Parsing command line arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
-use std::{io, error::Error};
+use std::{env, error::Error, fs::File};
 
 
-pub fn run() -> Result<(), Box<dyn Error>> {
-    let mut rdr = csv::Reader::from_reader(io::stdin());
+pub struct Config {
+    filename: String,
+}
+
+impl Config {
+    pub fn new(mut args: env::Args) -> Result<Self, &'static str> {
+        args.next();
+
+        let filename = match args.next() {
+            Some(f) => {f},
+            None => {return Err("Input csv file not specified.");},
+        };
+
+        Ok(Self{
+            filename,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file = File::open(config.filename)?;
+    let mut rdr = csv::Reader::from_reader(file);
 
     for record in rdr.records() {
         let parsed = record?;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::process;
+use std::{env, process};
+use csvtutor::Config;
 
 
 fn main() {
-    if let Err(e) = csvtutor::run() {
-        println!("Oh ho, kai zhaale?: {}", e);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Error reading input arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = csvtutor::run(config) {
+        eprintln!("Error reading file: {}", e);
         process::exit(1);
     }
 }
