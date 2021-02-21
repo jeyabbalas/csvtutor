2259f9d61ef2401dbcfaa12d9c15ddcc5999cfa1
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2021-02-21 00:15:11 -0500

    Writing with serde.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use serde::{Serialize, Deserialize};
 
 pub struct Config {
     filename: String,
+    output_file: String,
 }
 
 impl Config {
@@ -15,8 +16,14 @@ impl Config {
             None => {return Err("Input csv file not specified.");},
         };
 
+        let output_file = match args.next() {
+            Some(f) => {f},
+            None => {return Err("Output tsv file not specified.");},
+        };
+
         Ok(Self{
             filename,
+            output_file,
         })
     }
 }
@@ -32,14 +39,14 @@ struct UsPopRecord {
     state: String,
 }
 
-fn read_population_data(config: Config) -> Result<Vec<UsPopRecord>, Box<dyn Error>> {
+fn read_population_data(config: &Config) -> Result<Vec<UsPopRecord>, Box<dyn Error>> {
     let mut pop_data = Vec::new();
 
     // Configure csv reader: https://docs.rs/csv/1.1.5/csv/struct.ReaderBuilder.html 
     let mut rdr = csv::ReaderBuilder::new()
                     .delimiter(b',')
                     .has_headers(true)
-                    .from_path(config.filename)?;
+                    .from_path(&config.filename)?;
     
     for record in rdr.deserialize() {
         let parsed: UsPopRecord = record?;
@@ -66,9 +73,25 @@ fn population_gt(pop_data: Vec<UsPopRecord>, gt: u64) -> Vec<UsPopRecord> {
     subpop_data
 }
 
+fn write_population_data(config: &Config, pop_data: Vec<UsPopRecord>) -> Result<(), Box<dyn Error>> {
+    let mut wtr = csv::WriterBuilder::new()
+                .delimiter(b'\t')
+                .has_headers(true)
+                .from_path(&config.output_file)?;
+    
+    for item in pop_data {
+        wtr.serialize(item)?;
+    }
+
+    wtr.flush()?;
+
+    Ok(())
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let pop_data = read_population_data(config)?;
+    let pop_data = read_population_data(&config)?;
     let subpop = population_gt(pop_data, 50000);
+    write_population_data(&config, subpop)?;
 
     Ok(())
 }
